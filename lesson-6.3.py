5eb155fc8f77dc797e8f6b2cc9e4273faa52ab67
Belyi909 <khodorkovyacheslav@yandex.ru>
2023-09-26 13:41:52 +0300

    правка

--- lesson-6.3.py
+++ lesson-6.3.py
@@ -1,16 +1,19 @@
 # coding=windows-1251
 # 6 урок 3 задача
+print('Введите переменную "a"')
 a = int(input())
+print('Введите переменную "b"')
 b = int(input())
 cnt = 0
 while (a >= b):
     print("Введите переменную a меньше переменной b")
     a = int(input())
+    print("Введите переменную b больше переменной a")
     b = int(input())
 for i in range (a, b + 1):
     if i % 2 == 0:
         cnt += 1
         print(i, end = " ")
 print()
-print("Итог:", cnt)
+print(f"Итог:{cnt}")
 print()
